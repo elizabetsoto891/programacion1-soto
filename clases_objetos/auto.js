d443e59e34ff62aa1e392569ecb9b1fe87bdb5c5
elizabethsoto891 <margotsoto891@gmail.com>
2021-07-27 16:52:57 -0300

    clases

---
+++ clases_objetos/auto.js
@@ -0,0 +1,18 @@
+export default class Auto {
+    kilometro = null;
+
+
+    constructor()
+    {
+
+    }
+
+    mantenimiento_realizado()
+    {
+        
+        console.log(`
+            mantenimiento realizado en tiempo y forma
+            ${this.kilometro}
+        `)
+    }
+}
