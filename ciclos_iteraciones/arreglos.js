a5ecd01bfd23d3fbe3e8fa22f3f9174c2916e19e
elizabetsoto891 <84043461+elizabetsoto891@users.noreply.github.com>
2022-08-26 10:54:00 -0300

    Delete arreglos.js

--- ciclos_iteraciones/arreglos.js
+++
@@ -1,7 +0,0 @@
-const mostrar_arreglos = () => {
-    let miarreglos = [25,88,100,11];
-    console.log(miarreglos[3]);
-}
-
-const boton = document.querySelector("#btn_mostrar_arreglos");
-boton.addEventListener("click", mostrar_arreglos);
